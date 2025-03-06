1bca638b7607f35d0ef4914b4b9d6f03b675af44
KomaVR <komavr12454@gmail.com>
2025-03-05 19:31:33 -0800

    Update retrieve_link.py

--- retrieve_link.py
+++ retrieve_link.py
@@ -5,31 +5,7 @@ import pandas as pd
 from selenium import webdriver
 
 channels = [
-    'https://www.youtube.com/channel/UC2UcOi6RPQG5J3DovCre1JA/videos',
-    'https://www.youtube.com/channel/UCO4mttl54gQ0UW-DqyVrvLQ/videos',
-    'https://www.youtube.com/channel/UCuNY7uTtTLTCS34YZHdefjA/videos',
-    'https://www.youtube.com/channel/UCjQiROcVtb0ZqPpA-xE2flA/videos',
-    'https://www.youtube.com/user/silentmajorityhk/videos',
-    'https://www.youtube.com/user/albertip/videos',
-    'https://www.youtube.com/channel/UCFEfp4o-Q46liveICbhyCHw/videos',
-    'https://www.youtube.com/user/singtaotv/videos',
-    'https://www.youtube.com/user/cuachiufai/videos',
-    'https://www.youtube.com/channel/UCaSlyjhR4WC7QhYuaivxb6g/videos',
-    'https://www.youtube.com/user/veronbenny/videos',
-    'https://www.youtube.com/user/arsing1225/videos',
-    'https://www.youtube.com/channel/UCp1Y_N-FSsuYJ9UUBO7vWGQ/videos',
-    'https://www.youtube.com/channel/UCbSj5vM3ctjI7_OaKckMqDQ/videos',
-    'https://www.youtube.com/user/leetszking/videos',
-    'https://www.youtube.com/channel/UCIIwLX8REayuH-sNLf8c-3A/videos',
-    'https://www.youtube.com/channel/UCIV3Card7sB1UHLLG8gWNUQ/videos',
-    'https://www.youtube.com/user/jackyko1109/videos',
-    'https://www.youtube.com/channel/UCkO4xzcz9g6XJbcqEzfOqjw/videos',
-    'https://www.youtube.com/channel/UCFfLWSnUCblI4Lpsph7H1lA/videos',
-    'https://www.youtube.com/channel/UChsweFvHEManjCQruTQZkVA/videos',
-    'https://www.youtube.com/user/pennys627/videos',
-    'https://www.youtube.com/channel/UCkpEyjUMcrGuJz4j2HB0VUA/videos',
-    'https://www.youtube.com/channel/UCvlBe-TQfjLFINSSYQt9Tjg/videos',
-    'https://www.youtube.com/channel/UCTm6y67OtLNpYONtVi0Y2nQ/videos'
+    'https://youtube.com/@therealpartyyeep?si=nTwENmP7KeCl3X-z/videos',
     ]
 urls = {}
 
